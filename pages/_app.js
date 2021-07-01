a85e3b86eb11d87ac466b34db6db8d4b64273a85
sergiomata <32809578+sergiomata@users.noreply.github.com>
2021-07-01 12:39:20 -0500

    fix: aws exports

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import '../styles/globals.css'
-import config from '../src/aws-exports';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import config from '../aws-exports';
 
 Amplify.configure({
   ...config, ssr: true
 });
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
 
 export default MyApp
