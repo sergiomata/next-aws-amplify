d3150d6d7ac9acb3a02a7ba83866c5425c480be3
sergiomata <32809578+sergiomata@users.noreply.github.com>
2021-07-01 12:27:32 -0500

    feat: add pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import config from '../src/aws-exports';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+Amplify.configure({
+  ...config, ssr: true
+});
 
 export default MyApp
